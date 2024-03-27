2e8131d1a89a8c800eafafbdf1fdada70474dccf
mohanned-sameh <mohanned2678@gmail.com>
2024-03-27 17:15:50 +0200

    Update forms and routes in social media app

--- socialmedia/forms.py
+++ socialmedia/forms.py
@@ -8,21 +8,26 @@ TODO:
 """
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import (
+    StringField,
+    PasswordField,
+    SubmitField,
+    BooleanField,
+    TextAreaField,
+)
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from socialmedia.models import User
+from flask_login import current_user
 
 
 class PostForm(FlaskForm):
-    content = TextAreaField(
-        "Content", validators=[DataRequired(), Length(min=1, max=140)]
-    )
+    content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Post")
 
 
 class CommentForm(FlaskForm):
-    content = TextAreaField(
-        "Content", validators=[DataRequired(), Length(min=1, max=140)]
-    )
+    content = TextAreaField("Content", validators=[DataRequired()])
     submit = SubmitField("Comment")
 
 
@@ -30,15 +35,32 @@ class RegistrationForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
-    email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
-    confirm_password = StringField("Confirm Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
     submit = SubmitField("Sign Up")
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "Username already exists. Please choose a different one."
+            )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                "Email already exists. Please choose a different one."
+            )
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = StringField("Password", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
     submit = SubmitField("Login")
 
 
@@ -46,5 +68,24 @@ class UpdateAccountForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=2, max=20)]
     )
-    email = StringField("Email", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    picture = FileField(
+        "Update Profile Picture", validators=[FileAllowed(["jpg", "png"])]
+    )
     submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    "Username already exists. Please choose a different one."
+                )
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    "Email already exists. Please choose a different one."
+                )
--- socialmedia/routes.py
+++ socialmedia/routes.py
@@ -5,6 +5,13 @@ from flask import render_template, url_for, flash, redirect, request, abort
 from socialmedia import app, db, bcrypt
 from socialmedia.models import User, Post, PostComments
 from flask_login import login_user, current_user, logout_user, login_required
+from socialmedia.forms import (
+    LoginForm,
+    RegistrationForm,
+    UpdateAccountForm,
+    PostForm,
+    CommentForm,
+)
 
 
 @app.route("/")
@@ -22,14 +29,33 @@ def landing_page():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    # TODO: Create a form for the user to register
-    pass
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash("Your account has been created! You are now able to log in", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html", title="SocialMedia - Register", form=form)
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # TODO: Create a form for the user to login
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check email and password", "danger")
+    return render_template("login.html", title="SocialMedia - Login", form=form)
 
 
 @app.route("/logout")
