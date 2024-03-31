f21a6d1639152d281392276f28d48d22a077c473
mohanned-sameh <mohanned2678@gmail.com>
2024-03-31 11:30:27 +0200

    Add database creation in app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from socialmedia import app, db
 
 app.app_context().push()
+db.create_all()
 if __name__ == "__main__":
-    db.create_all()
     app.run()
--- createDB.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-from socialmedia import db, app
-
-app.app_context().push()
-db.create_all()
